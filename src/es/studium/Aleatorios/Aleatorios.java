b16842dc4053a63723176511740cf2fff9774754
Javier Martin <jamaga@outlook.es>
2019-11-27 09:21:29 +0100

    Firts Version

---
+++ src/es/studium/Aleatorios/Aleatorios.java
@@ -0,0 +1,13 @@
+package es.studium.Aleatorios;
+
+public class Aleatorios {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Número aleatorio:" + Math.random());
+		System.out.println("Otro número aleatorio: " + Math.random());
+
+	}
+
+}
